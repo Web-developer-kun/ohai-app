f9c919761be34b7547a80ae32bd77f78a7c3ff54
GitGudJavaScript <gitgudcasul@pm.me>
2019-06-21 14:59:16 +0100

    Install all relevant npm packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { fillRegisterForm } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger();
+const rootReducers = fillRegisterForm;
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/reducers.js
+++ src/reducers.js
@@ -1,13 +1,13 @@
 import {
+  SET_REGISTER_EMAIL,
   CHANGE_PASS_FIELD_1,
   CHANGE_PASS_FIELD_2,
-  SET_PASSWORD,
   SET_PASS_ERR,
-  SET_REGISTER_EMAIL,
-  CHANGE_ROUTE,
+  SET_PASSWORD,
   SET_LOGIN_EMAIL,
-  SET_LOGIN_PASSWORD
- } from './constants'
+  SET_LOGIN_PASSWORD,
+  CHANGE_ROUTE
+} from './constants'
 
 const initialRegisterState = {
   email: '',
@@ -19,10 +19,14 @@ const initialRegisterState = {
 
 export const fillRegisterForm = (state=initialRegisterState, action={}) => {
   switch (action.type) {
+    case SET_REGISTER_EMAIL:
+      return Object.assign({}, state, {email: action.payload})
     case CHANGE_PASS_FIELD_1:
       return Object.assign({}, state, {setPass1: action.payload})
     case CHANGE_PASS_FIELD_2:
       return Object.assign({}, state, {setPass2: action.payload})
+    case SET_PASS_ERR:
+      return Object.assign({}, state, {passErr: action.payload})
     case SET_PASSWORD:
       return Object.assign({}, state, {password: action.payload})
     default:
