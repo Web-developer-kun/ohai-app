1a6e34842d679525d695c354a5039df133d8afca
GitGudJavaScript <gitgudcasul@pm.me>
2019-06-27 14:32:10 +0100

    Handle signout client

--- src/components/Placeholder/Placeholder.js
+++ src/components/Placeholder/Placeholder.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 class Placeholder extends React.Component {
+    signOut = () => {
+      const { changeRoute } = this.props;
+      fetch('http://localhost:3000/signout', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              token: window.sessionStorage.getItem('token')
+            })
+          }).then(response => response.json())
+          .then(window.sessionStorage.removeItem('token'))
+          .then(changeRoute('signin'));
+    }
     render(){
         return(
-            <h1>Successful Login</h1>
+          <div>
+          <h1>Placeholder</h1>
+          <button
+            onClick={this.signOut}
+            className="btn btn-lg btn-primary btn-block"
+          >Sign Out</button>
+          </div>
         )
     }
 }
