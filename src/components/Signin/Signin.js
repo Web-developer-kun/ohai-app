98a8168661768c1e4aef9fcf2ccfae568cf4f632
mr-git-gud <gitgudcasul@pm.me>
2019-07-11 15:35:49 +0100

    Restyle login form for an animated background

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -106,49 +106,51 @@ class Signin extends React.Component {
     } = this.props;
     return (
       <div className="container">
-        <div className="text-center">
-          <h1 className="h3 mb-3 font-weight-normal header-logo">PingIM</h1>
-          <label className="form-err">
-            {formErrMsg.length ? formErrMsg : ""}
-          </label>
-          <input
-            type="email"
-            id="inputEmail"
-            className="form-control"
-            placeholder="Email address"
-            required=""
-            autoFocus=""
-            onChange={onSignInEmailChange}
-          />
-          <input
-            type="password"
-            id="inputPassword"
-            className="form-control"
-            placeholder="Password"
-            required=""
-            onChange={onSignInPasswordChange}
-          />
-        </div>
-        <div className="row">
-          <div className="col-sm"></div>
-          <div className="col-sm">
-            <button
-              onClick={this.onSignIn}
-              className="btn btn-lg btn-primary btn-block signin"
-            >
-              Sign In
-            </button>
-            <button
-              onClick={() => {
-                this.navigateToRegister();
-              }}
-              id="to-register"
-              className="btn btn-lg btn-primary btn-block register"
-            >
-              Register
-            </button>
+        <div className="login-form">
+          <div className="text-center">
+            <h1 className="h1 mb-3 font-weight-normal header-logo">PingIM</h1>
+            <label className="form-err">
+              {formErrMsg.length ? formErrMsg : ""}
+            </label>
+            <input
+              type="email"
+              id="inputEmail"
+              className="form-control"
+              placeholder="Email address"
+              required=""
+              autoFocus=""
+              onChange={onSignInEmailChange}
+            />
+            <input
+              type="password"
+              id="inputPassword"
+              className="form-control"
+              placeholder="Password"
+              required=""
+              onChange={onSignInPasswordChange}
+            />
           </div>
-          <div className="col-sm"></div>
+          <div className="row">
+            <div className="col-sm"></div>
+            <div className="col-sm">
+              <button
+                onClick={this.onSignIn}
+                className="btn btn-lg btn-primary btn-block signin"
+              >
+                Sign In
+              </button>
+              <button
+                onClick={() => {
+                  this.navigateToRegister();
+                }}
+                id="to-register"
+                className="btn btn-lg btn-primary btn-block register"
+              >
+                Register
+              </button>
+            </div>
+            <div className="col-sm"></div>
+          </div>
         </div>
       </div>
     );
