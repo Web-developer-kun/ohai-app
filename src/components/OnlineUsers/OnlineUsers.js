b2c86a2906e16c3e180e8ed9fffef8b2cc188525
mr-git-gud <gitgudcasul@pm.me>
2019-07-13 12:25:31 +0100

    Finish styling before the refactor

--- src/components/OnlineUsers/OnlineUsers.js
+++ src/components/OnlineUsers/OnlineUsers.js
@@ -13,24 +13,6 @@ const OnlineUsers = ({
   return (
     <div id="active-users" className="col-md-auto">
       <h6 id="online-users-header">Online:</h6>
-      {pmUserName.length ? (
-        <div
-          style={{ cursor: "pointer" }}
-          onClick={() => {
-            setPmSid("");
-            setPmUserName("");
-          }}
-        >
-          <div className="pm-alert" style={{ display: "inline-block" }}>
-            Messaging {pmUserName}
-          </div>
-          <div style={{ "margin-left": "10px", display: "inline-block" }}>
-            <FontAwesomeIcon className="pm-stop" icon={faTimes} size="1x" />
-          </div>
-        </div>
-      ) : (
-        ""
-      )}
       {connectedSockets.map((cs, i) => {
         return (
           <OnlineUser
--- src/components/TownSquare/TownSquare.js
+++ src/components/TownSquare/TownSquare.js
@@ -6,10 +6,12 @@ import ImageUploader from "../ImageUploader/ImageUploader";
 import Modal from "../Modal/Modal";
 import SignOut from "../SignOut/SignOut";
 import "../townsquare.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 class TownSquare extends React.Component {
   render() {
-    const { isModalOpen } = this.props;
+    const { isModalOpen, setPmSid, setPmUserName, pmUserName } = this.props;
     return (
       <div className="container-fluid">
         {isModalOpen ? (
@@ -18,8 +20,28 @@ class TownSquare extends React.Component {
           </Modal>
         ) : null}
         <div id="townSquareHeader" className="row">
-          <h3 className="col-3">PingIM</h3>
+          <h3 className="col-4">PingIM</h3>
+          {pmUserName.length ? (
+            <div
+              className="pm-notification-bar"
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                setPmSid("");
+                setPmUserName("");
+              }}
+            >
+              <div className="pm-alert" style={{ display: "inline-block" }}>
+                Messaging {pmUserName}. (Click here to cancel):
+              </div>
+              <div style={{ "margin-left": "10px", display: "inline-block" }}>
+                <FontAwesomeIcon className="pm-stop" icon={faTimes} size="1x" />
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
+
         <div className="row">
           <ChatBox {...this.props} />
           <OnlineUsers {...this.props} />
